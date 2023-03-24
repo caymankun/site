1d24abd9955d1f7ab51b2d5690f7bff7c0eb1933
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-24 09:53:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+} )(jQuery);
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+}
