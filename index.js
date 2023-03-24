24d1c741708669ae1af9e735597de922158e9d2d
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-25 08:13:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-(function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open'; // class
-  // menu open close
-  $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-      $nav.addClass( open );
-    } else {
-      $nav.removeClass( open );
-    }
-  });
-  // mask close
-  $mask.on('click', function() {
-    $nav.removeClass( open );
-  });
-} )(jQuery);
+$(function(){
+    $(".btn-gnavi").on("click", function(){
+        // ハンバーガーメニューの位置を設定
+        var rightVal = 0;
+        if($(this).hasClass("open")) {
+            // 位置を移動させメニューを開いた状態にする
+            rightVal = -300;
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).removeClass("open");
+        } else {
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).addClass("open");
+        }
+        $("#global-navi").stop().animate({
+            right: rightVal
+        }, 200);
+    });
+});
 
 $(function(){
  
