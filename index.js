2ed99608797b8b7b61480d4eaf6e91364b145ac3
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-25 12:16:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ $(function(){
 
 
 
-function() {
+function foo () {
     if (document.getElementById("check").checked) {
       document.removeEventListener('touchmove', noscroll);
     document.removeEventListener('wheel', noscroll);
