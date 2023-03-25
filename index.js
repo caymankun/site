c31e12642430b15d4b19c7178497c5aa8687a865
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-25 11:43:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,28 @@ $(function(){
 
 });
 
+
+
+
+
+// マウスホイールを有効にするかどうか
+let enableMouseWheel = true;
+
+// チェックボックスをクリックした時の処理
+document
+.querySelector('.c-switch-label"')
+.addEventListener('click',(event)=>{
+  enableMouseWheel = event.target.checked === false;
+});
+
+// スクロール可能な要素上でマウスホイールした時の処理
+document
+.querySelector('.headhn')
+.addEventListener('wheel',(event) =>{
+  if(enableMouseWheel === true){
+    return;
+  }
+  // マウスホイールが無効な場合はevent.preventDefault()を実行
+  event.preventDefault();
+});
+
